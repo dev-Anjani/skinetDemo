f3d334f4772b6486e4023b4216fb653e6afb2f38
dev-AkB <dev-AkB@users.noreply.github.com>
2020-05-24 13:26:26 +0530

    Section12 complted

--- client/src/app/core/server-error/server-error.component.ts
+++ client/src/app/core/server-error/server-error.component.ts
@@ -12,7 +12,7 @@ export class ServerErrorComponent implements OnInit {
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();
     this.error = navigation && navigation.extras && navigation.extras.state && navigation.extras.state.error;
-    
+
    }
 
   ngOnInit(): void {
